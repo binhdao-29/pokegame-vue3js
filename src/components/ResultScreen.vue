cfbbec9c045c89ca56db704ae9e6a66d75edd908
binhdao29 <binhchan0293@gmail.com>
2021-07-14 10:49:07 +0700

    feat: score

--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="result-container">
     <div class="heading">✨ Congratulations ✨</div>
-    <div class="subheading">{{ Math.round(timer / 1000) }} seconds</div>
+    <div class="content">
+      <div class="subheading">Time: {{ Math.round(timer / 1000) }} seconds</div>
+      <div class="subheading">Score: {{ Math.round(timer / 190) }}</div>
+    </div>
     <button class="btn-play" @click="onStartAgain">Play again</button>
   </div>
 </template>
@@ -41,7 +44,7 @@ export default {
   );
 
   .btn-play {
-    width: 8rem;
+    width: 10.5rem;
     height: 3rem;
     background-color: var(--dark);
     border: 1px solid var(--dark);
@@ -63,12 +66,21 @@ export default {
     }
   }
 
+  .content {
+    margin-top: 1rem;
+    text-align: left;
+  }
+
   .heading {
     font-size: 2.5rem;
 
     @media screen and (min-width: 576px) {
       font-size: 4rem;
     }
+  }
+
+  .subheading {
+    margin: 0.5rem 0;
   }
 }
 </style>
